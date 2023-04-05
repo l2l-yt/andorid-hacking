105ab2228d26357f23d1716ee9b69846e0106ae4
l2l-yt <129922659+l2l-yt@users.noreply.github.com>
2023-04-05 08:41:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function login() {
+    // get the username and password
+    var username = document.getElementsByName("username")[0].value;
+    var password = document.getElementsByName("password")[0].value;
+    
+    // check if the username and password are correct
+    if (username === "hkffking@gmial.com" && password === "427475426") {
+      alert("Login successful!");
+      
+    } else {
+      alert("Username or password is incorrect.");
+    console.log("fuck your!");
+      function khan(haris){
+        console.log("this is called me ", a)
+        alert("this is called me ", a)
+      }
+      khan("Hariskhan")
+    }
+  }
